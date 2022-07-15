0bdd9c65a6c7a3c16e514964b8d6a4c120c06a08
KYapp <kukiyomenaisapp@gmail.com>
2022-07-15 15:30:28 +0900

    Init

---
+++ JConsole/JConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Kokuban;
+
+namespace JuhaKurisu.JConsole
+{
+    public static class JConsole
+    {
+        public static void Init()
+        {
+            Console.WriteLine(Console.WindowHeight);
+            Console.WriteLine(Console.WindowWidth);
+            Console.WriteLine("\u001b[31mHello World!\u001b[0m");
+            var stdout = Console.OpenStandardOutput();
+            var con = new StreamWriter(stdout, Encoding.ASCII);
+            con.AutoFlush = true;
+            Console.SetOut(con);
+
+            Console.WriteLine("\x1b[36mTEST\x1b[0m");
+            Console.Write(Chalk.Gray + "test");
+            Console.Write("test2");
+            Console.ReadKey();
+        }
+
+        public static void Update()
+        {
+
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,3 @@
+﻿using JuhaKurisu.JConsole;
+
+JConsole.Init();
