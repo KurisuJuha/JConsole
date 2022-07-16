e60e8eb2d561215a5631809612a3142bb0c8fad1
KYapp <kukiyomenaisapp@gmail.com>
2022-07-16 18:42:12 +0900

    _displayの内容を表示するようにした。

--- JConsole/JConsole.cs
+++ JConsole/JConsole.cs
@@ -33,19 +33,34 @@ namespace JuhaKurisu.JConsole
                 Parallel.For(0, Console.WindowHeight, i =>
                 {
                     _display[i] = new char[Console.WindowWidth];
+
+                    Parallel.For(0, Console.WindowWidth, j =>
+                    {
+                        if (_display[i][j] == '\0')
+                        {
+                            _display[i][j] = ' ';
+                        }
+                    });
                 });
 
                 lateWindowHeight = Console.WindowHeight;
                 lateWindowWidth = Console.WindowWidth;
+                _display[0][0] = 'a';
+                _display[3][10] = 'a';
             }
 
+            // _displayの内容を文字列化
+            string[] ss = new string[_display.Length];
+            for (int i = 0; i < ss.Length; i++)
+            {
+                ss[i] = string.Join("", _display[i]);
+            }
+            Console.Write(string.Join('\n', ss));
 
 
             sw.Stop();
             TimeSpan span = sw.Elapsed;
             //            Console.WriteLine(1 / span.TotalSeconds);
-            Console.WriteLine(lateWindowHeight);
-            Console.WriteLine(lateWindowWidth);
         }
     }
 }
