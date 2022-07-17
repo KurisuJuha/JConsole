0dbb12c3155f4d4d2cd86c1cd4d7c7436b2345bc
KYapp <kukiyomenaisapp@gmail.com>
2022-07-17 18:39:48 +0900

    tryで囲んで実行時に失敗しても止まらないようにした。

--- JConsole/JConsole.cs
+++ JConsole/JConsole.cs
@@ -24,53 +24,60 @@ namespace JuhaKurisu.JConsole
 
         public static void Update()
         {
-            sw.Restart();
-            Console.SetCursorPosition(0, 0);
-
-            // _displayを初期化
-            if (lateWindowHeight != Console.WindowHeight || lateWindowWidth != Console.WindowWidth)
+            try
             {
-                Console.Clear();
-                _display = new string[Console.WindowHeight][];
+                sw.Restart();
+                Console.SetCursorPosition(0, 0);
 
-                for (int i = 0; i < Console.WindowHeight; i++)
+                // _displayを初期化
+                if (lateWindowHeight != Console.WindowHeight || lateWindowWidth != Console.WindowWidth)
                 {
-                    _display[i] = new string[Console.WindowWidth];
+                    Console.Clear();
+                    _display = new string[Console.WindowHeight][];
 
-                    for (int j = 0; j < Console.WindowWidth; j++)
+                    for (int i = 0; i < Console.WindowHeight; i++)
                     {
-                        _display[i][j] = Display.GetBDisplay(j, i).ToString();
+                        _display[i] = new string[Console.WindowWidth];
+
+                        for (int j = 0; j < Console.WindowWidth; j++)
+                        {
+                            _display[i][j] = Display.GetBDisplay(j, i).ToString();
+                        }
                     }
+
+                    lateWindowHeight = Console.WindowHeight;
+                    lateWindowWidth = Console.WindowWidth;
                 }
 
-                lateWindowHeight = Console.WindowHeight;
-                lateWindowWidth = Console.WindowWidth;
-            }
-
-            // displayのdiffを元に_displayを変更する
-            for (int i = 0; i < Display.Diff.Count; i++)
-            {
-                if (_display.Length > Display.Diff[i].Item2 && _display[0].Length > Display.Diff[i].Item1)
+                // displayのdiffを元に_displayを変更する
+                for (int i = 0; i < Display.Diff.Count; i++)
                 {
-                    _display[Display.Diff[i].Item2][Display.Diff[i].Item1] = Display.GetBDisplay(Display.Diff[i].Item1, Display.Diff[i].Item2).ToString();
+                    if (_display.Length > Console.WindowHeight - 1 - Display.Diff[i].Item2 && _display[0].Length > Display.Diff[i].Item1)
+                    {
+                        _display[Console.WindowHeight - 1 - Display.Diff[i].Item2][Display.Diff[i].Item1] = Display.GetBDisplay(Display.Diff[i].Item1, Console.WindowHeight - 1 - Display.Diff[i].Item2).ToString();
+                    }
+
+                    Display.ResetDiff();
                 }
 
-                Display.ResetDiff();
+                // _displayの内容を文字列化
+                string[] ss = new string[_display.Length];
+
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    ss[i] = string.Join("", _display[i]);
+                }
+                Console.Write(string.Join('\n', ss));
+
+
+                sw.Stop();
+                TimeSpan span = sw.Elapsed;
+                //            Console.WriteLine(1 / span.TotalSeconds);
             }
-
-            // _displayの内容を文字列化
-            string[] ss = new string[_display.Length];
-
-            for (int i = 0; i < ss.Length; i++)
+            catch (Exception ex)
             {
-                ss[i] = string.Join("", _display[i]);
-            }
-            Console.Write(string.Join('\n', ss));
-            
 
-            sw.Stop();
-            TimeSpan span = sw.Elapsed;
-            //            Console.WriteLine(1 / span.TotalSeconds);
+            }
         }
     }
 }
